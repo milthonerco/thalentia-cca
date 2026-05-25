c42c02caed7298ddfb2f28a0f0fc8461b99cf084
milthonerco <duvanmunoz@cca.edu.co>
2026-05-25 14:23:51 -0500

    Modificamos para nuestro primero despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel
+from "@astrojs/vercel/server";
 export default defineConfig({
+  output: "server",
+
+  adapter:
+  vercel(),
   integrations: [tailwind()],
   
 });
